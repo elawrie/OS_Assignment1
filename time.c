44586b3bfe6a69e56f88747f8bdea037f96438aa
Evelyn Lawrie <lawrie@chapman.edu>
2024-02-19 15:15:46 -0800

    adds outline to time file

---
+++ time.c
@@ -0,0 +1,13 @@
+// set up general stuff in a c program 
+
+// do the program logic
+
+// use code snippit provided:
+struct timeval current;
+gettimeofday(&current,NULL);
+// current.tv sec represents seconds
+// current.tv usec represents microseconds
+
+// do error handling (make sure user enters a valid command)
+
+// fart
